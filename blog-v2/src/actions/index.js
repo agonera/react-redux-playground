5501e61ffb5ffebab317fa92b2c809c96bce9bbb
Aleksandra Gonera <goneraleksandra@gmail.com>
2019-04-21 12:43:36 +0200

    handled duplicates with lodash

--- blog-v2/src/actions/index.js
+++ blog-v2/src/actions/index.js
@@ -1,6 +1,13 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+
+export const fetchPostsAndUsers = () => async dispatch => {
+    // we take argument 'dispatch' so we can dispatch our own actions inside of here
+    // call fetchPosts once and fetchUsers multiple times
+};
+
+
 // export const fetchPosts = () => {
 //     return async function (dispatch, getState) {
 //         const response = await jsonPlaceholder.get('/posts');
@@ -22,15 +29,18 @@ export const fetchPosts = () => async dispatch => { // it returns a request obje
 };
 
 // id - id of the user we want to fetch
-export const fetchUser = (id) => dispatch => {
-    _fetchUser(id, dispatch);
-};
-
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
     const response = await jsonPlaceholder.get('/users/' + id);
     dispatch({
         type: 'FETCH_USER',
         payload: response.data
-    });
-});
+    })
+};
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get('/users/' + id);
+//     dispatch({
+//         type: 'FETCH_USER',
+//         payload: response.data
+//     });
+// });
